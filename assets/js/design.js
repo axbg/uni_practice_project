3e265c09596d4f476df3241ba4f105a141826fd8
Alex Bisag <bisagalexstefan@gmail.com>
2018-04-25 19:53:44 +0300

    Full responsiveness

--- assets/js/design.js
+++ assets/js/design.js
@@ -1,9 +1,9 @@
 
 window.onscroll = function() {fixedNav()};
 
-var navbar = document.getElementsByTagName('nav')[0];
+let navbar = document.getElementsByTagName('nav')[0];
 
-var sticky = navbar.offsetTop;
+let sticky = navbar.offsetTop;
 
 function fixedNav() {
     if (window.pageYOffset >= sticky) {
@@ -11,4 +11,22 @@ function fixedNav() {
     } else {
         navbar.classList.remove("sticky");
     }
+}
+
+function openModal(element){
+
+    let number = element.getAttribute("number");
+
+    let modalTitle = document.getElementById("modalTitle");
+    let modalBody = document.getElementById("modalBody");
+
+    let titleContent = document.getElementById("card-title-" + number);
+    let cardContent = document.getElementById("card-text-" + number);
+
+    modalTitle.innerText = "";
+    modalBody.innerText = "";
+
+    modalTitle.innerText = titleContent.innerText;
+    modalBody.innerText = cardContent.innerText;
+
 }
